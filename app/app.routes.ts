7786d5d1b095dac992276aa4ad9960e22c8d5e44
DAlinka <90259926+drahosalinka@users.noreply.github.com>
2024-04-17 18:11:24 +0200

    Add files via upload

---
+++ app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { UserListComponent } from './user-list/user-list.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: UserListComponent
+
+    }
+];
---
+++ app/user-list/user-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { UserDTO } from '../../../models';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent implements OnInit {
+  userService = inject(UserService);
+
+  users: UserDTO[] = [];
+
+  ngOnInit(): void {
+    this.userService.getAll().subscribe({
+      next: users => this.users = users,
+      error: err => console.error(err)
+    });
+  }
+}
